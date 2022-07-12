9293c969dd7cdad171f01f7a75ab9720ce018006
Srinath <srinathrajath0@gmail.com>
2022-07-12 15:24:48 +0530

    first commmit

---
+++ one.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+read -p "username : " user_var
+read -sp "password : " pass_var
+echo
+echo "username : $user_var"
+echo "password : $pass_var"
---
+++ three.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+git init
+git add .
+git commit -m "first commmit"
+git remote add origin https://github.com/srinathrajath/project3.git
+git push origin master or git push -f origin master
+
