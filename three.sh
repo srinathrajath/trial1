195b94e3449686845790c25394adf130a415e61d
Srinath <srinathrajath0@gmail.com>
2022-07-12 15:36:20 +0530

    [message]

--- three.sh
+++ three.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-
 git init
 git add .
-git commit -m "first commmit"
-git remote add origin https://github.com/srinathrajath/project3.git
-git push origin master or git push -f origin master
-
+git commit -m "[message]"
+git branch -M master
+git remote add origin "https://github.com/srinathrajath/project3.git"
+git push -u origin master
